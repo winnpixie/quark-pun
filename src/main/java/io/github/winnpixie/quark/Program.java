12abdfd6e044ed49192291d833a6f142f01b76e1
Hannah <salvadin2@gmail.com>
2023-09-09 08:17:49 -0400

    first commit

---
+++ src/main/java/io/github/winnpixie/quark/Program.java
@@ -0,0 +1,93 @@
+package io.github.winnpixie.quark;
+
+import java.io.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class Program {
+    public static boolean RUNNING = true;
+
+    public static void main(String[] args) {
+        // CONNECTION TESTING
+
+        try {
+            DatagramSocket udpSock = new DatagramSocket(5055);
+            byte[] buffer = new byte[8192];
+            DatagramPacket incomingPacket = new DatagramPacket(buffer, buffer.length);
+
+            while (RUNNING) {
+                System.out.println("WAITING");
+                udpSock.receive(incomingPacket);
+
+                byte[] incomingPayload = incomingPacket.getData();
+                int len = incomingPacket.getLength();
+
+                int time;
+                int challenge;
+                try (DataInputStream dis = new DataInputStream(new ByteArrayInputStream(incomingPayload, 0, len))) {
+                    short peerId = dis.readShort();
+                    System.out.printf("PEER_ID: %d%n", peerId);
+
+                    byte crc = dis.readByte();
+                    System.out.printf("CRC: %d%n", crc);
+
+                    byte cmdCount = dis.readByte();
+                    System.out.printf("CMD_C: %d%n", cmdCount);
+
+                    time = dis.readInt();
+                    System.out.printf("TIME: %d%n", time);
+
+                    challenge = dis.readInt();
+                    System.out.printf("CHALLENGE: %d%n", challenge);
+                }
+
+                System.out.printf("Payload (%d bytes)%n", len);
+                for (int i = 0; i < len; i++) {
+                    System.out.printf("%02X, ", incomingPayload[i]);
+                }
+
+                System.out.printf("%nrecv from %s%n", incomingPacket.getAddress().getHostAddress());
+
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                try (DataOutputStream dos = new DataOutputStream(baos)) {
+                    // PACKET HEADER
+                    dos.writeShort(1); // ???
+                    dos.writeByte(0); // CRC
+                    dos.writeByte(1); // Command Count
+                    dos.writeInt(time); // Time
+                    dos.writeInt(challenge); // Challenge
+
+                    // TYPES
+                    // 3 = Connect?
+                    // 4 = Disconnect
+
+                    // RESERVED
+                    // Disconnect
+                    //  1 == Server Logic
+                    //  3 == Server Full
+
+                    // Payload
+
+                    // COMMAND
+                    dos.writeByte(4); // Command Id
+                    dos.writeByte(0); // Channel Id
+                    dos.writeByte(0); // Flags
+                    dos.writeByte(3); // Reserved
+                    dos.writeInt(2); // Size
+                    dos.writeInt(0); // Reliable Seq Num
+
+                    // dos.writeShort(42);
+
+                    dos.flush();
+                }
+
+                byte[] outgoingPayload = baos.toByteArray();
+                DatagramPacket outgoingPacket = new DatagramPacket(outgoingPayload, 0, outgoingPayload.length,
+                        incomingPacket.getAddress(), incomingPacket.getPort());
+                udpSock.send(outgoingPacket);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
